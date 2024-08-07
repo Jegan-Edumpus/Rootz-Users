a04e2e36d348882cc0efce4ab4f8488027daac25
Dhanapal03 <dhanapal@digispoc.com>
2024-08-07 20:03:49 +0530

    count api helpers added

---
+++ utils/helper.js
@@ -0,0 +1,66 @@
+const { default: axios } = require("axios");
+const createError = require("http-errors");
+
+async function getPostCount({ user_id = null }) {
+  try {
+    if (!user_id) {
+      throw new Error(createError(400, "user_id is required"));
+    }
+    const postData = await axios.post(
+      `${process.env.POSTS_BASEURL}/post-count`,
+      {
+        user_id: [user_id],
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (postData.data.statusCode === 200) {
+      const count = postData.data?.count || null;
+      return count;
+    } else if (postData.data.statusCode === 400) {
+      return null;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.log("fetch post count error --------", error);
+    throw new Error(createError(500, error));
+  }
+}
+
+async function getConnectionCount({ user_id = null }) {
+  try {
+    if (!user_id) {
+      throw new Error(createError(400, "user_id is required"));
+    }
+    const result = await axios.post(
+      `${process.env.CONNECTIONS_BASEURL}/connection-count`,
+      {
+        user_id,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (result.data.statusCode === 200) {
+      const count = result.data?.count || null;
+      return count;
+    } else if (result.data.statusCode === 400) {
+      return null;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.log("fetch connection count error --------", error);
+    throw new Error(createError(500, error));
+  }
+}
+module.exports = {
+  getPostCount,
+  getConnectionCount,
+};
