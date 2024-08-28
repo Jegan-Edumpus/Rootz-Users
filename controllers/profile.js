24394af908569476bdd7d6ba8a6f5725d6b089c3
Dhanapal03 <dhanapal@digispoc.com>
2024-08-28 17:34:43 +0530

    username added

--- controllers/profile.js
+++ controllers/profile.js
@@ -64,11 +64,13 @@ const profileDetails = async (req, res, next) => {
           "UPDATE users SET user_name=? WHERE id=? and deleted_at is null",
           [userName?.length ? userName[0] : null, id]
         );
+        user_name = userName?.[0] || null;
       }
 
       return res.status(200).json({
         profileDetails: {
           ...profile[0],
+          user_name,
           image: signedUrl,
           blocked: blocked && blocked !== "NULL" ? blocked.split(",") : [],
           flag: flag?.emoji,
